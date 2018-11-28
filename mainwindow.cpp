e7e32b411dd24c440707ce29ce205f59c6fe7091
wh230308 <wh08420@outlook.com>
2018-11-28 16:04:43 +0800

    创建LIU监控程序Qt QWindow界面工程

---
+++ mainwindow.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setMinimumSize(800, 600);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
